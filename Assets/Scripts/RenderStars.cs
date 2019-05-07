856dc11f04ce2857a0a0845f99fdda137de3586c
Chris Hart <chart@concord.org>
2019-05-07 16:46:26 -0400

    Initial commit

---
+++ Assets/Scripts/RenderStars.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderStars : MonoBehaviour
+{
+    public GameObject star;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (star != null){
+            Instantiate(star, transform.position, transform.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
