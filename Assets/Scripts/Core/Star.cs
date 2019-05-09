18e7036f87f38731467c790ec6be314fd56e4834
Chris Hart <chris@codetorque.com>
2019-05-09 11:05:37 -0400

    Import data from text files and load into objects

---
+++ Assets/Scripts/Core/Star.cs
@@ -0,0 +1,24 @@
+﻿public class Star{
+    public string ID; //right now, this is a sequence of characters that combines Bayer and Flamsteed star id
+    public string Constellation; //univeral 3-character constellation abbrievation
+    public float RA; //right ascension in decimal hours
+    public float Dec; //declanation in degrees, positive for northern, negative for southern, 0 to 90
+    public float Dist; //star distance in parsecs
+    public float Mag; //apparent magnitude (smallest numbers, e.g., negative numbers, are the brightest)
+    public float Alt; //Altitude
+    public float Azm; //Azimuth
+
+    // other properties for a star
+    public float size;
+    public float y;
+    public float x;
+    public float z;
+    public bool hidden;
+
+    public string XByerFlamsteed;
+}
+
+public class Marker{
+    public float RA;
+    public float Dec;
+}
---
+++ Assets/Scripts/DataController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataController : MonoBehaviour
+{
+    public TextAsset starData;
+    public TextAsset cityData;
+
+    [SerializeField]
+    private List<Star> allStars;
+    public List<Star> AllStars
+    {
+        get { return allStars;}
+        private set { allStars = value;}
+    }
+
+    [SerializeField]
+    private List<City> allCities;
+    public List<City> AllCities
+    {
+        get { return allCities;}
+        private set { allCities = value;}
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (starData != null){
+            allStars = DataImport.ImportStarData(starData.text);
+            Debug.Log(allStars.Count + " stars imported");
+        }
+        if(cityData != null){
+            allCities = DataImport.ImportCityData(cityData.text);
+            Debug.Log(allCities.Count + " cities imported");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
