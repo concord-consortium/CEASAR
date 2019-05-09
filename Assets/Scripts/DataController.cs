cf0a0e7823ebe5ac2ff91a67dcba6ab274cf7ede
Chris Hart <chart@concord.org>
2019-05-09 11:23:30 -0400

    Render a star for each data point - random locations for now

--- Assets/Scripts/DataController.cs
+++ Assets/Scripts/DataController.cs
@@ -11,29 +11,44 @@ public class DataController : MonoBehaviour
     private List<Star> allStars;
     public List<Star> AllStars
     {
-        get { return allStars;}
-        private set { allStars = value;}
+        get { return allStars; }
+        private set { allStars = value; }
     }
 
     [SerializeField]
     private List<City> allCities;
     public List<City> AllCities
     {
-        get { return allCities;}
-        private set { allCities = value;}
+        get { return allCities; }
+        private set { allCities = value; }
     }
 
+    public GameObject dataStarPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (starData != null){
+        if (starData != null)
+        {
             allStars = DataImport.ImportStarData(starData.text);
             Debug.Log(allStars.Count + " stars imported");
         }
-        if(cityData != null){
+        if (cityData != null)
+        {
             allCities = DataImport.ImportCityData(cityData.text);
             Debug.Log(allCities.Count + " cities imported");
+        }
+
+        if (dataStarPrefab != null && allStars != null && allStars.Count > 0)
+        {
+            foreach (Star dataStar in allStars)
+            {
+                GameObject starObject = Instantiate(dataStarPrefab, Random.onUnitSphere * 50, Quaternion.identity);
+                starObject.transform.LookAt(transform);
+                StarComponent newStar = starObject.GetComponent<StarComponent>();
+                newStar.starData = dataStar;
+                starObject.name = dataStar.Constellation;
+            }
         }
     }
 
