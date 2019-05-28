53d9c51ce5864b1cf0a657b72181a51a2af91bbb
mklewandowski <matt.kain.lewandowski@gmail.com>
2019-05-28 13:38:59 -0700

    Optimize update calls to LookAt in FaceCamera script

--- Assets/Scripts/FaceCamera.cs
+++ Assets/Scripts/FaceCamera.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class FaceCamera : MonoBehaviour
 {
+    private Vector3 currentPosition;
+    private Quaternion currentRotation;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        currentPosition = this.transform.position;
+        currentRotation = this.transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(Camera.main.transform);
+        // if the scale of the parent changes, then so does the position
+        if (currentPosition != this.transform.position || currentRotation != this.transform.rotation)
+        {
+            currentPosition = this.transform.position;
+            currentRotation = this.transform.rotation;
+            transform.LookAt(Camera.main.transform);
+        }
     }
 }
