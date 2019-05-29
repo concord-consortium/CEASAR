d567280809ec471825cc6efe204b925a6b1a363f
mklewandowski <matt.kain.lewandowski@gmail.com>
2019-05-29 01:19:59 -0700

    Allow date and time of horizon view to be specified by user

---
+++ Assets/Scripts/TextFromSlider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextFromSlider : MonoBehaviour
+{
+    public int decimalDigits = 2;
+
+    private TextMeshProUGUI TMText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TMText = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TextUpdate(float val)
+    {
+        string formatter = "F" + decimalDigits.ToString();
+        TMText.text = val.ToString(formatter);
+    }
+}
