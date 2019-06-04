95c51b9febd279c11e729bcb2ae2e7621f2e6e64
mklewandowski <matt.kain.lewandowski@gmail.com>
2019-06-04 11:19:03 -0700

    Move UI scripts to UI folder, refactor function names for consistency

---
+++ Assets/Scripts/UI/SimulationRateDropdown.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SimulationRateDropdown : MonoBehaviour
+{
+    public GameObject dataController;
+    TMP_Dropdown dropdown;
+
+    void Start()
+    {
+        dropdown = GetComponent<TMP_Dropdown>();
+        //Add listener for when the value of the Dropdown changes, to take action
+        dropdown.onValueChanged.AddListener(delegate {
+            DropdownValueChanged(dropdown);
+        });
+    }
+
+    void DropdownValueChanged(TMP_Dropdown change)
+    {
+        if (dataController)
+        {
+            string newScale = change.captionText.text.Remove(change.captionText.text.Length - 1, 1);
+            dataController.GetComponent<DataController>().SetSimulationTimeScale((float) System.Convert.ToDouble(newScale));
+        }
+    }
+
+}
---
+++ Assets/Scripts/UI/UIControlCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControlCamera : MonoBehaviour
+{
+    Camera mainCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void LookUpDown(float speed)
+    {
+        mainCam.transform.Rotate(Vector3.left, speed * Time.deltaTime, Space.Self);
+        // this.transform.Rotate(Vector3.down, rotateSpeed * Time.deltaTime);
+    }
+    public void LookLeftRight(float speed)
+    {
+        mainCam.transform.Rotate(Vector3.up, speed * Time.deltaTime, Space.World);
+    }
+
+}
