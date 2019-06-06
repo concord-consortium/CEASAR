eed88d0bfd55cdc06edef98f4ff477ab4cb154ae
mklewandowski <matt.kain.lewandowski@gmail.com>
2019-06-06 00:53:04 -0700

    Update content displayed in star info panel

--- Assets/Scripts/StarInfoPanel.cs
+++ Assets/Scripts/StarInfoPanel.cs
@@ -5,23 +5,9 @@ using TMPro;
 
 public class StarInfoPanel : MonoBehaviour
 {
-    public TextMeshProUGUI starXByerFlamsteedText;
-    public TextMeshProUGUI starMagText;
-    public TextMeshProUGUI starConstellationText;
+    public TextMeshProUGUI starInfoText;
 
     private bool isEnabled = true;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     public void setEnabled(bool enabled)
     {
@@ -36,14 +22,31 @@ public class StarInfoPanel : MonoBehaviour
         }
     }
 
-    public void UpdateStarInfoPanel(string XByerFlamsteed, string magnitude, string constellation)
+    public void UpdateStarInfoPanel(Star starData)
     {
         if (isEnabled && !gameObject.activeSelf)
         {
             gameObject.SetActive(true);
         }
-        starXByerFlamsteedText.text = "ID: " + XByerFlamsteed;
-        starMagText.text = "Mag: " + magnitude;
-        starConstellationText.text = "Const: " + constellation;
+        string description = "";
+        description += "Name: ";
+        description += starData.ProperName.Length > 0 ? starData.ProperName : "N/A";
+        description += "\n";
+        description += "Hipparcos #: ";
+        description += starData.Hipparcos.ToString();
+        description += "\n";
+        description += "Bayer Des.: ";
+        description += starData.BayerDesignation.Length > 0 ? starData.BayerDesignation : "N/A";
+        description += "\n";
+        description += "Flamsteed Des.: ";
+        description += starData.FlamsteedDesignation.Length > 0 ? starData.FlamsteedDesignation : "N/A";;
+        description += "\n";
+        description += "Constellation: ";
+        description += starData.ConstellationFullName.Length > 0 ? starData.ConstellationFullName : "N/A";
+        description += "\n";
+        description += "m: ";
+        description += starData.Mag.ToString();
+        description += "\n";
+        starInfoText.text = description;
     }
 }
