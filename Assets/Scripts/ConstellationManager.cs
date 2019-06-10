479cad973d48f354e52e393f156d3ea8d8b25800
mklewandowski <matt.kain.lewandowski@gmail.com>
2019-06-09 18:20:04 -0700

    Add ConstellationManager class used to store Constellation objects and functions to highlight and draw constellations

---
+++ Assets/Scripts/ConstellationManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstellationManager : MonoBehaviour
+{
+    List<Constellation> constellations = new List<Constellation>();
+
+    public void AddConstellation(Constellation constellation)
+    {
+        constellations.Add(constellation);
+    }
+
+	public Constellation GetConstellation(string cname)
+	{
+		int index = constellations.FindIndex(el => el.constellationNameAbbr == cname);
+        if (index < 0)
+        {
+            return constellations[index];
+        }
+        else
+        {
+            return null;
+        }
+	}
+
+	public void HighlightSingleConstellation(string cname)
+	{
+        foreach (var constellation in constellations)
+        {
+            constellation.Highlight(constellation.constellationNameAbbr == cname);
+            constellation.ShowConstellationLines(constellation.constellationNameAbbr == cname);
+        }
+	}
+
+    public void HighlightAllConstellations(bool highlight)
+	{
+        foreach (var constellation in constellations)
+        {
+            constellation.Highlight(highlight);
+            constellation.ShowConstellationLines(highlight);
+        }
+	}
+
+	public void ShowSingleConstellation(string cname)
+	{
+        foreach (var constellation in constellations)
+        {
+            constellation.ShowConstellationLines(constellation.constellationNameAbbr == cname);
+        }
+	}
+
+    public void ShowAllConstellations()
+	{
+        foreach (var constellation in constellations)
+        {
+            constellation.ShowConstellationLines(true);
+        }
+	}
+
+}
