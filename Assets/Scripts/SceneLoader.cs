b65562b6582fca798bacef79ab6ca838b24b02c7
mklewandowski <matt.kain.lewandowski@gmail.com>
2019-06-13 08:48:13 -0700

    adjust button text, add shortcut keys for level load

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -9,6 +9,27 @@ public class SceneLoader : MonoBehaviour
     public GameObject LoadingPanel;
     public GameObject ButtonPanel;
     public Slider slider;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            LoadScene(1);
+        }
+        else if (Input.GetKeyDown("2"))
+        {
+            LoadScene(2);
+        }
+        else if (Input.GetKeyDown("3"))
+        {
+            LoadScene(3);
+        }
+        else if (Input.GetKeyDown("4"))
+        {
+            LoadScene(4);
+        }
+    }
     public void LoadScene (int sceneIndex)
     {
         StartCoroutine(LoadAsynchronously(sceneIndex));
