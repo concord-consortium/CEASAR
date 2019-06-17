3e0bfe361759725d3ba0360950915db1d57200c5
Chris Hart <chart@concord.org>
2019-06-17 10:33:37 -0400

    Add a basic Celestial Sphere prefab to facilitate creating new scenes Add some non-essential but nice-to-have options to create a plain script-free field of stars, and a skybox of stars

---
+++ Assets/Scripts/CleanStars.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CleanStars : MonoBehaviour
+{
+    public bool cleanStars = false;
+    private bool hasCleanedStars = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasCleanedStars && cleanStars)
+        {
+            foreach (StarComponent star in FindObjectsOfType<StarComponent>())
+            {
+                Destroy(star.GetComponent<SphereCollider>());
+                Destroy(star);
+            }
+            foreach (Constellation constellation in FindObjectsOfType<Constellation>())
+            {
+                Destroy(constellation);
+            }
+            foreach (ConstellationsController controller in FindObjectsOfType<ConstellationsController>())
+            {
+                Destroy(controller);
+            }
+            foreach (DataController data in FindObjectsOfType<DataController>())
+            {
+                Destroy(data);
+            }
+            Destroy(GameObject.Find("Markers"));
+            Debug.Log("Clean!");
+            hasCleanedStars = true;
+        }
+    }
+}
