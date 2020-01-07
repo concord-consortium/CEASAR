1ec4b33e0f55d6bd831540d337dc3142158dcdc1
Chris Hart <chart@concord.org>
2020-01-07 16:57:42 -0500

    Improve switching cameras for platforms that have a VR device attached
    Also adjust UI height upwards to ensure lower portion is still visible on Horizon view

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -14,9 +14,31 @@ public class SceneLoader : MonoBehaviour
     private GameObject vrEventSystem;
     [SerializeField]
     private GameObject defaultEventSystem;
+
     public void Start()
     {
-#if UNITY_ANDROID
+#if UNITY_ANDROID || UNITY_STANDALONE_WIN
+        string model = UnityEngine.XR.XRDevice.model != null ? UnityEngine.XR.XRDevice.model : "";
+        if (!string.IsNullOrEmpty(model))
+        {
+            // we have an XR device attached! Oculus Quest is detected as Rift S if it is connected with USB3 Oculus Link
+            // TODO: If we detect an HTC Vive, Valve Index, or other headset, we need to do more work to use different controllers
+            Debug.Log("Detected XR device: " + model);
+            setupXRCameras();
+        } 
+        else
+        {
+            // assume no headset plugged in or available - this may be an Android AR build?
+            setupStandardCameras();
+        }
+#else
+       setupStandardCameras();
+#endif
+
+    }
+
+    private void setupXRCameras()
+    {
         GameObject existingCamera = GameObject.FindGameObjectWithTag("MainCamera");
         if (existingCamera != null)
         {
@@ -27,7 +49,7 @@ public class SceneLoader : MonoBehaviour
         foreach (Canvas c in mainUI)
         {
             c.renderMode = RenderMode.WorldSpace;
-            c.transform.position = new Vector3(0, 2, 5);
+            c.transform.position = new Vector3(0, 3, 5);
             c.transform.localScale = new Vector3(0.007f, 0.007f, 0.007f);
             c.planeDistance = 10;
             c.worldCamera = GameObject.Find("CenterEyeAnchor").GetComponent<Camera>();
@@ -47,19 +69,16 @@ public class SceneLoader : MonoBehaviour
         {
             horizonCamControls.SetActive(false);
         }
-
-#else
-       
+    }
+    private void setupStandardCameras()
+    {
         GameObject existingCamera = GameObject.FindGameObjectWithTag("MainCamera");
-        if (existingCamera == null && regularCameraRig != null){
+        if (existingCamera == null && regularCameraRig != null)
+        {
             GameObject mainCam = new GameObject("MainCamera", typeof(Camera));
             mainCam.tag = "MainCamera";
             mainCam.GetComponent<Camera>().clearFlags = CameraClearFlags.SolidColor;
             mainCam.GetComponent<Camera>().backgroundColor = Color.black;
         }
-
-#endif
-
     }
-
 }
