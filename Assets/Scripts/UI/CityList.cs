c262ff09609d03ee150012dbfbd2ab046fff83f5
Chris Hart <chart@concord.org>
2021-01-15 12:08:49 -0500

    Updated City list instead of drop down

--- Assets/Scripts/UI/CityList.cs
+++ Assets/Scripts/UI/CityList.cs
@@ -10,10 +10,8 @@ public class CityList : MonoBehaviour
     private List<string> allCityNames;
     [SerializeField] private GameObject cityTextObject;
     [SerializeField] private RectTransform contentContainer;
-    [SerializeField] private RectTransform spawnPoint;
     public void InitCityNames(List<string> cityNames, string currentCity)
     {
-        contentContainer.sizeDelta = new Vector2(0, cityNames.Count * 20);     
         if (allCityNames == null || allCityNames.Count == 0)
         {
             string[] cities = cityNames.ToArray();
@@ -22,11 +20,8 @@ public class CityList : MonoBehaviour
                 GameObject cityObject = Instantiate(cityTextObject);
                 cityObject.transform.SetParent(contentContainer);
                 cityObject.transform.localScale = Vector3.one;
-                Vector3 p = cityObject.transform.localPosition;
-                cityObject.transform.localPosition = new Vector3(p.x, p.y, -0.1f);
-                
-                cityObject.GetComponent<TMP_Text>().text = cities[i];
-                cityObject.name = cities[i];
+                bool isSelected = SimulationManager.Instance.CurrentLocationName == cities[i];
+                cityObject.GetComponent<CityItem>().Init(cities[i], isSelected);
             }
 
             // cache the list of names
