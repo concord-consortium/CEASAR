4ef6273c12d253e3a6e4387e23bb71c9f246bc9e
Chris Hart <chart@concord.org>
2021-02-01 16:17:32 -0500

    Left and right hand control fixed for menu and info panel
    Also hide / shrink / move local player avatar if it exists (suspect it creeps in when we're not looking)

---
+++ Assets/Scripts/Network/AvatarController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AvatarController : MonoBehaviour
+{
+    [SerializeField] float avatarScale = 1.0f;
+    [SerializeField] Vector3 horizonSceneAvatarOffset;
+
+    void Start()
+    {
+        this.transform.localScale = Vector3.one * avatarScale;
+        if (SceneManager.GetActiveScene().name == SimulationConstants.SCENE_HORIZON)
+        {
+            this.transform.localPosition = Vector3.zero + horizonSceneAvatarOffset;
+        }
+    }
+
+}
