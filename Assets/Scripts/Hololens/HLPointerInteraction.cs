37621bcb899327b057692037230f56b0e4d67275
Chris Hart <chart@concord.org>
2021-02-05 12:18:50 -0500

    Fix star pointer interaction in Horizon

---
+++ Assets/Scripts/Hololens/HLPointerInteraction.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit;
+using UnityEngine;
+
+public class HLPointerInteraction : IMixedRealityPointerHandler, IMixedRealityFocusHandler
+{
+
+    void IMixedRealityFocusHandler.OnFocusEnter(FocusEventData eventData)
+    {
+        
+    }
+
+    void IMixedRealityFocusHandler.OnFocusExit(FocusEventData eventData)
+    {
+        
+    }
+
+    void IMixedRealityPointerHandler.OnPointerClicked(MixedRealityPointerEventData eventData)
+    {
+        CCDebug.Log(eventData.Count, LogLevel.Verbose, LogMessageCategory.VR);
+        CCDebug.Log("PointerClicked " + eventData.Pointer.Result, LogLevel.Display, LogMessageCategory.VR);
+       
+    }
+
+    void IMixedRealityPointerHandler.OnPointerDown(MixedRealityPointerEventData eventData)
+    {
+
+        CCDebug.Log(eventData, LogLevel.Display, LogMessageCategory.VR);
+
+    }
+
+    void IMixedRealityPointerHandler.OnPointerDragged(MixedRealityPointerEventData eventData)
+    {
+        CCDebug.Log(eventData, LogLevel.Verbose, LogMessageCategory.VR);
+    }
+
+    void IMixedRealityPointerHandler.OnPointerUp(MixedRealityPointerEventData eventData)
+    {
+        CCDebug.Log(eventData, LogLevel.Verbose, LogMessageCategory.VR);
+    }
+}
---
+++ Assets/Scripts/Hololens/HLTouchInteraction.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HLTouchInteraction : MonoBehaviour, IMixedRealityInputHandler, IMixedRealityTouchHandler
+{
+    private StarComponent sc;
+    void OnEnable()
+    {
+        sc = GetComponent<StarComponent>();
+    }
+    void IMixedRealityInputHandler.OnInputDown(InputEventData eventData)
+    {
+        if (sc)
+        {
+            CCDebug.Log(name, LogLevel.Display, LogMessageCategory.VR);
+            if (sc)
+            {
+                sc.HandleSelectStar(true);
+            }
+        }
+
+        // CCDebug.Log("InputDown " + eventData.SourceId, LogLevel.Display, LogMessageCategory.VR);
+
+    }
+
+    void IMixedRealityInputHandler.OnInputUp(InputEventData eventData)
+    {
+        CCDebug.Log(eventData.InputSource, LogLevel.Verbose, LogMessageCategory.VR);
+        CCDebug.Log("InputUp " + eventData.InputSource, LogLevel.Display, LogMessageCategory.VR);
+    }
+
+    void IMixedRealityTouchHandler.OnTouchCompleted(HandTrackingInputEventData eventData)
+    {
+
+        CCDebug.Log(eventData, LogLevel.Verbose, LogMessageCategory.VR);
+        CCDebug.Log("TouchCompleted " + eventData.InputData, LogLevel.Display, LogMessageCategory.VR);
+       
+
+    }
+
+    void IMixedRealityTouchHandler.OnTouchStarted(HandTrackingInputEventData eventData)
+    {
+        CCDebug.Log(eventData, LogLevel.Verbose, LogMessageCategory.VR);
+
+    }
+
+    void IMixedRealityTouchHandler.OnTouchUpdated(HandTrackingInputEventData eventData)
+    {
+        // CCDebug.Log("TouchUpdated " + eventData.InputSource.Pointers[0].Position, LogLevel.Verbose, LogMessageCategory.VR);
+        // CCDebug.Log("TouchUpdated " + eventData.InputSource.Pointers[0].Position, LogLevel.Display, LogMessageCategory.VR);
+        Vector3 touchPosition = eventData.InputSource.Pointers[0].Position;
+        SimulationManager.Instance.InteractionControllerObject.GetComponent<InteractionController>().SetEarthLocationPin(touchPosition);
+    }
+}
